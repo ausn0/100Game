b5fd58a9b8b45e7730bee97e5468c028327b9423
ausn0 <a.usn675@gmail.com>
2024-03-28 14:44:41 -0400

    CLI version working

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import time
+
 
 response= requests.get(url="https://en.wikipedia.org/wiki/Lists_of_women")
 print(response.status_code)
@@ -10,6 +12,7 @@ print(title.string)
 
 
 #List of ALL LISTS OF WOMEN
+timer1 = time.time()
 allLinks = soup.find(id="mw-content-text").find_all('a')
 #print(allLinks)
 masterWomenList = []
@@ -41,10 +44,36 @@ for womanList in allLinks:
             "|(by|wmf)|(Upload|ISSN)")
         if(re.search(blacklist, woman) ):
             continue
-        print(woman)
+        if(woman.isascii()==-1):
+            continue
+        #print(woman)
+        woman = woman.lower()
         masterWomenList.append(woman)
         #success
 
+#CLI game for now
+timer2=time.time()
+print("Woman database complete")
+elapsed = timer2-timer1
+print("Time elapsed: ", elapsed)
+womanCount = 0
+guessed = []
+confirm = input("Ready to begin? Press Enter")
+
+clock1 = time.time()
+while womanCount <100:
+    guess = input(f"Woman Count:\n {womanCount}" )
+    guess = guess.lower()
+
+    #If correct and not in guessed list, increment womanCount
+    if((guess in masterWomenList) and guess not in guessed):
+        guessed.append(guess)
+        womanCount += 1
+
+clock2 = time.time()
+score = clock2 - clock1
+print("Congratulations, you have named 100 women.")
+print(f'Time: {score}')
 
 #print(masterWomenList)
 
